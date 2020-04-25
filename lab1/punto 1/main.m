bdb52fd349f11c7a594ab2dfaf2c999b084d01d8
Diego <juanmedina0505@gmail.com>
2020-04-25 18:42:23 -0500

    Primer Punto Lab1 completado :tada::tada::tada:

--- lab1/punto 1/main.m
+++ lab1/punto 1/main.m
@@ -1,8 +1,46 @@
-clear, clc, close all 
+clc
+pkg load signal
 
-x = -5 : .01 : 5;
-y = caja(x);
-plot(x,y)
+#Se lee la funcion definida por el usuario
+while(true)
+  try 
+   f =  input ('ingrese la funcion con parametro t: ', 's');
 
-xlabel('x')
-ylabel('Square Wave')
+    break;
+  catch
+    printf("Favor ingresar un funcion valida\n");
+  end_try_catch
+endwhile 
+
+#Se lee el periodo T definido por el usuario
+while(true)
+  try 
+   T= input ('ingrese el periodo T: ');
+   T= T+0;
+    break;
+  catch
+    printf("Favor ingresar un valor valido\n");
+  end_try_catch
+endwhile 
+
+#Se lee el N definido por el usuario
+while(true)
+  try 
+  N= input('ingrese N: ');
+  N= N+0;
+    break;
+  catch
+    printf("Favor ingresar un valor valido\n");
+  end_try_catch
+endwhile 
+
+# con base en el periodo se define un arreglo con abscisas que permitan discretizar la funcion
+t= [-T/2:0.01:T/2]; 
+
+#funcion que contiene la logica de la serie de fourier
+serie_fourier= fourier(f,T,t,N);
+
+# se grafican los resultados
+g = arrayfun(inline(f),t);
+plot(t,g,t,(a0/2 + serie_fourier),'r');
+title(['serie de Fourier trigonometrica para ', f]); grid on; %grafico de la serie de Fourier contra la funcion f(t)
