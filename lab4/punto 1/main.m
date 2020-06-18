d73b70152225a00bee00a1b2d2713011bf12cb81
Diego <juanmedina0505@gmail.com>
2020-06-17 23:13:26 -0500

    Lab 4 punto 1 terminado :tada:

--- lab4/punto 1/main.m
+++ lab4/punto 1/main.m
@@ -3,24 +3,25 @@
 while(true)
   try 
    d =  input ('ingrese la matriz de datos: ');
-
-    break;
+   if size(d,2) != 3
+     error();
+   endif
+   for i= 1:size(d,1)
+     for j= 1:size(d,2)
+       if !(d(i,j) == 1 | d(i,j) == 0)
+         error();
+       endif
+     endfor
+   endfor
+    
+   break;
   catch
-    printf("Favor ingresar un funcion valida\n");
+    printf("Favor ingresar un Matriz valida con un tamaño valido\n");
   end_try_catch
 endwhile 
 
 
-#Se lee el matriz de errores
-while(true)
-  try 
-   err =  input ('ingrese el matriz de errores: ');
 
-    break;
-  catch
-    printf("Favor ingresar un funcion valida\n");
-  end_try_catch
-endwhile 
 
 
 printf("______________________________________________\n \n \n ");  
@@ -56,7 +57,7 @@ printf("Posibles Palabras del Codigo \n \n");
 printf("--------------------------------------\n");  
 printf("%10s %10s %10s \n","di","ci","w(ci)");
 printf("--------------------------------------\n");  
-c = [];# posibes palabras
+c = [];# vector que almacena las posibes palabras
 for i=0:(2**k-1)
   [di,s_di]= toBinary(i,k);
   ci = multiplicacionBinaria(di,G);
@@ -74,15 +75,93 @@ printf("______________________________________________\n \n \n ");
 
 
 
-# se imita el comportamiento de un Discrete Memoryless Channel
-r = sumaBinaria(C,err);# se recibe C mas un error  | C + err
 
-    # r = [0 1 0 1 1 1];% MENSAJE RECIBIDO  | EJEMPLO CLASE%
- 
-H_t = [     P'; 
+
+printf("\n");  
+printf("Opciones de Decodificacion \n");  
+printf("  1) Usar la matriz codificada C que pase por un Discrete Memoryless Channel\n");
+printf("  2) Ingresar Valor a decodificar \n");
+
+
+while(true)
+try
+  sel_dec = input ("opcion: ");
+  if (sel_dec == 1 || sel_dec == 2)
+    break
+  endif
+  printf("Ingrese una Opcion valida\n");
+catch
+  printf("Ingrese una Opcion valida\n");
+end_try_catch
+endwhile
+
+
+
+
+if sel_dec == 1
+  #Se lee el matriz de errores despues de pasar C por el DMC
+  while(true)
+    try 
+     err =  input ('ingrese el matriz de errores asociada al DMC  r = C + e: ');
+     if size(err,2) != 6
+       error();
+     endif
+     for i= 1:size(err,1)
+       for j= 1:size(err,2)
+         if !(err(i,j) == 1 || err(i,j) == 0)
+           error();
+         endif
+       endfor
+     endfor
+     break;
+    catch
+      printf("Favor ingresar un Matriz valida\n");
+    end_try_catch
+  endwhile 
+else
+    #Se lee el matriz de de datos codificada 
+  while(true)
+    try 
+     r_opt2 =  input ('ingrese la matriz de datos a decodificar: ');
+     if size(r_opt2,2) != 6
+       error();
+     endif
+     for i= 1:size(r_opt2,1)
+       for j= 1:size(r_opt2,2)
+         if !(r_opt2(i,j) == 1 || r_opt2(i,j) == 0)
+           error();
+         endif
+       endfor
+     endfor
+     break;
+    catch
+      printf("Favor ingresar un Matriz valida\n");
+    end_try_catch
+  endwhile 
+endif
+
+
+
+
+# Se elige que decodificar de acuerdo a la opcion elegida
+if sel_dec == 1
+   r = sumaBinaria(C,err);# se recibe C mas un error  | C + err
+else
+  r = r_opt2;
+endif
+
+
+
+
+H_t = [     P'; # Matriz de chequeo de paridad
        eye(size(P,2))]; 
+       r
+       H_t
 S = multiplicacionBinaria(r,H_t); # Sindrome   
 [r_ne,r_d,err] = decodificacion(r,P,H_t,S);
+
+
+
 printf("Matriz  Recibida | Palabras que se reciben del DMC \n \n");
 disp(r);
 for i = 1:length(err)
